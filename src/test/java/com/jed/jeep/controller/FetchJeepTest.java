7cecd6bd714bf26ffe45aaf0935fd247553fd90c
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2021-04-15 10:40:59 -0700

    added webenvironment random port to Fetchjeep test class

--- src/test/java/com/jed/jeep/controller/FetchJeepTest.java
+++ src/test/java/com/jed/jeep/controller/FetchJeepTest.java
@@ -1,8 +1,12 @@
 package com.jed.jeep.controller;
 
+import com.jed.jeep.controller.support.BaseTest;
+import com.jed.jeep.controller.support.FetchJeepTestSupport;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-public class FetchJeepTest {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class FetchJeepTest extends FetchJeepTestSupport {
   @Test
   void name() {}
 }
