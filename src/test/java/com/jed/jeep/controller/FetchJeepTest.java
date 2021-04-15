0406d7ad182e606d3c98ac6ac92316eed54a3c4c
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2021-04-15 11:14:57 -0700

    created jeepmodel enum. tested and working. green means go

--- src/test/java/com/jed/jeep/controller/FetchJeepTest.java
+++ src/test/java/com/jed/jeep/controller/FetchJeepTest.java
@@ -2,13 +2,23 @@ package com.jed.jeep.controller;
 
 import com.jed.jeep.controller.support.BaseTest;
 import com.jed.jeep.controller.support.FetchJeepTestSupport;
+import com.jed.jeep.entities.JeepModel;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class FetchJeepTest extends FetchJeepTestSupport {
+
   @Test
-  void name() {
+  void testThatJeepsAreReturnedWhenAValidModelAndTrimAreSupplied() {
+
+
+    //Given a valid model, trim, and URI
+    JeepModel model = JeepModel.WRANGLER;
+
+    //When a connection is made to the URI
+
+    //Then a valid response code is returned 200 - OK
 
     System.out.println(getBaseUri());
   }
