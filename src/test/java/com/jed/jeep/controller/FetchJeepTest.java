2898136124fbd966af71048ae5d2f2a1a1a1c8e5
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2021-04-15 16:32:01 -0700

    added restcontroller to BasicJeepSalesController class

--- src/test/java/com/jed/jeep/controller/FetchJeepTest.java
+++ src/test/java/com/jed/jeep/controller/FetchJeepTest.java
@@ -6,6 +6,7 @@ import com.jed.jeep.entity.JeepModel;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -17,10 +18,11 @@ public class FetchJeepTest extends FetchJeepTestSupport {
     // Given a valid model, trim, and URI
 
     JeepModel model = JeepModel.MODEL_ID;
+
     JeepModel trim = JeepModel.TRIM_LEVEL;
     String uri = String.format("%s?model=%s&trim=%s", getBaseUri(), model, trim);
 
-    //    System.out.println(uri);
+        System.out.println(uri);
 
     // When a connection is made to the URI
         ResponseEntity<Jeep> res = getRestTemplate().getForEntity(uri, Jeep.class);
@@ -31,7 +33,7 @@ public class FetchJeepTest extends FetchJeepTestSupport {
 
     // Then a valid response code is returned 200 - OK
 
-    assert (res.getStatusCode().is2xxSuccessful());
+    assert (res.getStatusCode().equals(HttpStatus.OK));
 
     System.out.println(getBaseUri());
   }
