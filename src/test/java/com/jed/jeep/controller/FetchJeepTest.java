985fa4556632b97d2b8f3b8bb6f9cd43773d9b11
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2021-04-15 15:52:29 -0700

    added openapi definition. failed

--- src/test/java/com/jed/jeep/controller/FetchJeepTest.java
+++ src/test/java/com/jed/jeep/controller/FetchJeepTest.java
@@ -15,18 +15,19 @@ public class FetchJeepTest extends FetchJeepTestSupport {
   void testThatJeepsAreReturnedWhenAValidModelAndTrimAreSupplied() {
 
     // Given a valid model, trim, and URI
-    JeepModel model = JeepModel.MODEL_PK;
-    String trim = "Sport";
+
+    JeepModel model = JeepModel.MODEL_ID;
+    JeepModel trim = JeepModel.TRIM_LEVEL;
     String uri = String.format("%s?model=%s&trim=%s", getBaseUri(), model, trim);
 
     //    System.out.println(uri);
 
     // When a connection is made to the URI
-    //    ResponseEntity<Jeep> res = getRestTemplate().getForEntity(uri, Jeep.class);
+        ResponseEntity<Jeep> res = getRestTemplate().getForEntity(uri, Jeep.class);
 
-    ResponseEntity<Jeep> res =
-        getRestTemplate()
-            .getForEntity(uri, Jeep.class, model, trim, new ParameterizedTypeReference<>() {});
+//    ResponseEntity<Jeep> res =
+//        getRestTemplate()
+//            .getForEntity(uri, Jeep.class, model, trim, new ParameterizedTypeReference<>() {});
 
     // Then a valid response code is returned 200 - OK
 
