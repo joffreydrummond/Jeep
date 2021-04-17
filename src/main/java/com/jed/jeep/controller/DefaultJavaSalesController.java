32b8a4572822d61b56c5a918598ecc1e416a7942
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2021-04-17 12:00:15 -0700

    added slf4j annotation

--- src/main/java/com/jed/jeep/controller/DefaultJavaSalesController.java
+++ src/main/java/com/jed/jeep/controller/DefaultJavaSalesController.java
@@ -1,14 +1,19 @@
 package com.jed.jeep.controller;
 
 import com.jed.jeep.entity.Jeep;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@RestController @Slf4j
 public class DefaultJavaSalesController implements JeepSalesController{
+
+
     @Override
     public List<Jeep> fetchJeeps(String model, String trim) {
+
+
         return null;
     }
 }
