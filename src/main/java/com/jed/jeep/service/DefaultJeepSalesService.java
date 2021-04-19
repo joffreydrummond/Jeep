804fbafa6914306bf371bba974857186d0bbcaa8
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-04-18 22:09:50 -0700

    added service annotation to DefaultJeepSalesService

--- src/main/java/com/jed/jeep/service/DefaultJeepSalesService.java
+++ src/main/java/com/jed/jeep/service/DefaultJeepSalesService.java
@@ -1,14 +1,18 @@
 package com.jed.jeep.service;
 
 import com.jed.jeep.entity.Jeep;
+import io.swagger.v3.oas.annotations.servers.Server;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 @Slf4j
 public class DefaultJeepSalesService implements JeepSalesService{
     @Override
     public List<Jeep> fetchJeeps(String model, String trim) {
+        log.info("The fetchJeeps method was called with model={}, trim={}", model, trim);
         return null;
     }
 }
