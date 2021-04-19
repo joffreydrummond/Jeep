d6831f2f6f94da01b1c4b4e855c70b5feb8fc162
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-04-18 21:11:11 -0700

    created link list. added jeep variables to list in buildexpected

--- src/test/java/com/jed/jeep/controller/support/FetchJeepTestSupport.java
+++ src/test/java/com/jed/jeep/controller/support/FetchJeepTestSupport.java
@@ -1,17 +1,26 @@
 package com.jed.jeep.controller.support;
 
 import com.jed.jeep.entity.Jeep;
+import com.jed.jeep.entity.JeepModel;
 
+import java.math.BigDecimal;
 import java.util.LinkedList;
 import java.util.List;
 
 public class FetchJeepTestSupport extends BaseTest {
 
   protected List<Jeep> buildExpected() {
-List<Jeep> list = new LinkedList<>();
+    List<Jeep> list = new LinkedList<>();
 
-Jeep j1 = Jeep.builder().build();
+    list.add(
+        Jeep.builder()
+            .modelId(JeepModel.WRANGLER)
+            .trimLevel("Sport")
+            .numDoors(2)
+            .wheelSize(17)
+            .basePrice(new BigDecimal("28475.00"))
+            .build());
 
-  return list;
+    return list;
   }
 }
