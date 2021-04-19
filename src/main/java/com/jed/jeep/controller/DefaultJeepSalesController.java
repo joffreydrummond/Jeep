d2a92932c416fb96d66424fa58b335d0590c2baa
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-04-18 22:04:09 -0700

    created jeelsaleservice interface

--- src/main/java/com/jed/jeep/controller/DefaultJeepSalesController.java
+++ src/main/java/com/jed/jeep/controller/DefaultJeepSalesController.java
@@ -1,17 +1,22 @@
 package com.jed.jeep.controller;
 
 import com.jed.jeep.entity.Jeep;
+import com.jed.jeep.service.JeepSalesService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 
 @RestController @Slf4j
 public class DefaultJeepSalesController implements JeepSalesController {
+  @Autowired
+  private JeepSalesService jeepSalesService;
+
   @Override
   public List<Jeep> fetchJeeps(String model, String trim) {
     log.debug("model={}, trim={}", model, trim);
-    return null;
+    return jeepSalesService.fetchJeeps(model, trim);
   }
 }
 
