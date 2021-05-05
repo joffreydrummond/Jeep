cdc931cd2d49b75ad63eb4d8e1e4ce96c1344219
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-04 21:37:40 -0700

    created new JeepOrderDao because package was not being found by springboot. queried sql for jeep specs. fail assertion

--- src/main/java/com/jed/jeep/controller/DefaultJeepSalesController.java
+++ src/main/java/com/jed/jeep/controller/DefaultJeepSalesController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController @Slf4j
 public class DefaultJeepSalesController implements JeepSalesController {
+
   @Autowired
   private JeepSalesService jeepSalesService;
 
--- src/main/java/com/jed/jeep/service/DefaultJeepSalesService.java
+++ src/main/java/com/jed/jeep/service/DefaultJeepSalesService.java
@@ -1,9 +1,11 @@
 package com.jed.jeep.service;
 
+import com.jed.jeep.dao.JeepOrderDao;
 import com.jed.jeep.entity.Jeep;
 import com.jed.jeep.entity.JeepModel;
 import io.swagger.v3.oas.annotations.servers.Server;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,9 +13,12 @@ import java.util.List;
 @Service
 @Slf4j
 public class DefaultJeepSalesService implements JeepSalesService{
+    @Autowired
+    private JeepOrderDao jeepOrderDao;
+
     @Override
     public List<Jeep> fetchJeeps(JeepModel model, String trim) {
         log.info("The fetchJeeps method was called with model={}, trim={}", model, trim);
-        return null;
+        return jeepOrderDao.fetchJeeps(model, trim);
     }
 }
