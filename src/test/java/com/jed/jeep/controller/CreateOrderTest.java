8bab65f1feb509f4e857e8a668dd1718d88b6110
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-27 21:06:40 -0700

    added annotations needed sql, activeprofiles, springboottest

--- src/test/java/com/jed/jeep/controller/CreateOrderTest.java
+++ src/test/java/com/jed/jeep/controller/CreateOrderTest.java
@@ -1,4 +1,17 @@
 package com.jed.jeep.controller;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@Sql(
+        scripts = {
+                "classpath:flyway/migrations/V1.0__Jeep_Schema.sql",
+                "classpath:flyway/migrations/V1.1__Jeep_Data" + ".sql"
+        },
+        config = @SqlConfig(encoding = "utf-8"))
 public class CreateOrderTest {
 }
