0035038c68d08c3046101051b2bb1f73b49648b5
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-29 13:24:23 -0700

    added service annotation

--- src/main/java/com/jed/jeep/dao/JeepOrderDao.java
+++ src/main/java/com/jed/jeep/dao/JeepOrderDao.java
@@ -2,9 +2,13 @@ package com.jed.jeep.dao;
 
 import com.jed.jeep.entity.Jeep;
 import com.jed.jeep.entity.JeepModel;
+import com.jed.jeep.entity.Order;
+import com.jed.jeep.entity.OrderRequest;
 
 import java.util.List;
 
 public interface JeepOrderDao {
     List<Jeep> fetchJeeps(JeepModel model, String trim);
+
+    Order createOrder(OrderRequest orderRequest);
 }
--- src/main/java/com/jed/jeep/service/DefaultJeepOrderService.java
+++ src/main/java/com/jed/jeep/service/DefaultJeepOrderService.java
@@ -1,11 +1,18 @@
 package com.jed.jeep.service;
 
+import com.jed.jeep.dao.JeepOrderDao;
 import com.jed.jeep.entity.Order;
 import com.jed.jeep.entity.OrderRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class DefaultJeepOrderService implements JeepOrderService{
+    @Autowired
+    private JeepOrderDao jeepOrderDao;
+
     @Override
     public Order createOrder(OrderRequest orderRequest) {
-        return null;
+        return jeepOrderDao.createOrder(orderRequest);
     }
 }
