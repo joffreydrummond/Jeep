2ff4dd183f9ae4fa0062c9e758a2582a54ff269a
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-29 13:22:31 -0700

    created DefaultJeepOrderService

---
+++ src/main/java/com/jed/jeep/service/DefaultJeepOrderService.java
@@ -0,0 +1,11 @@
+package com.jed.jeep.service;
+
+import com.jed.jeep.entity.Order;
+import com.jed.jeep.entity.OrderRequest;
+
+public class DefaultJeepOrderService implements JeepOrderService{
+    @Override
+    public Order createOrder(OrderRequest orderRequest) {
+        return null;
+    }
+}
