4afc36be78be35b1f1662faac06d8b6793e0bde2
Joffrey <45638283+joffreydrummond@users.noreply.github.com>
2021-05-29 14:24:49 -0700

    fixed option to class objects for customer, jeep, color engine, tire

--- src/main/java/com/jed/jeep/service/DefaultJeepOrderService.java
+++ src/main/java/com/jed/jeep/service/DefaultJeepOrderService.java
@@ -6,105 +6,120 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
-public class DefaultJeepOrderService implements JeepOrderService{
-    @Autowired
-    private JeepOrderDao jeepOrderDao;
+public class DefaultJeepOrderService implements JeepOrderService {
+  @Autowired private JeepOrderDao jeepOrderDao;
 
-    /**
-     *
-     * @param orderRequest
-     * @return
-     */
-    private List<Option> getOption(OrderRequest orderRequest) {
-        return jeepOrderDao.fetchOptions(orderRequest.getOptions());
+  /**
+   * @param orderRequest
+   * @return
+   */
+  private List<Option> getOption(OrderRequest orderRequest) {
+    return jeepOrderDao.fetchOptions(orderRequest.getOptions());
+  }
+
+  /**
+   * @param orderRequest
+   * @return
+   */
+  protected Tire getTire(OrderRequest orderRequest) {
+    return jeepOrderDao
+        .fetchTire(orderRequest.getTire())
+        .orElseThrow(
+            () ->
+                new NoSuchElementException(
+                    "Tire with ID=" + orderRequest.getTire() + " was not found"));
+  }
+
+  /**
+   * @param orderRequest
+   * @return
+   */
+  protected Engine getEngine(OrderRequest orderRequest) {
+    return jeepOrderDao
+        .fetchEngine(orderRequest.getEngine())
+        .orElseThrow(
+            () ->
+                new NoSuchElementException(
+                    "Engine with ID=" + orderRequest.getEngine() + " was not found"));
+  }
+
+  /**
+   * @param orderRequest
+   * @return
+   */
+  protected Color getColor(OrderRequest orderRequest) {
+    return jeepOrderDao
+        .fetchColor(orderRequest.getColor())
+        .orElseThrow(
+            () ->
+                new NoSuchElementException(
+                    "Color with ID=" + orderRequest.getColor() + " was not found"));
+  }
+
+  /**
+   * @param orderRequest
+   * @return
+   */
+  protected Jeep getModel(OrderRequest orderRequest) {
+    return jeepOrderDao
+        .fetchModel(orderRequest.getModel(), orderRequest.getTrim(), orderRequest.getDoors())
+        .orElseThrow(
+            () ->
+                new NoSuchElementException(
+                    "Model with ID="
+                        + orderRequest.getModel()
+                        + ", trim="
+                        + orderRequest.getTrim()
+                        + orderRequest.getDoors()
+                        + " was not found"));
+  }
+
+  /**
+   * @param orderRequest
+   * @return
+   */
+  protected Customer getCustomer(OrderRequest orderRequest) {
+    return jeepOrderDao
+        .fetchCustomer(orderRequest.getCustomer())
+        .orElseThrow(
+            () ->
+                new NoSuchElementException(
+                    "Customer with ID=" + orderRequest.getCustomer() + " was not found"));
+  }
+
+  @Transactional
+  @Override
+  public Order createOrder(OrderRequest orderRequest) {
+    Customer customer = getCustomer(orderRequest);
+    Jeep jeep = getModel(orderRequest);
+    Color color = getColor(orderRequest);
+    Engine engine = getEngine(orderRequest);
+    Tire tire = getTire(orderRequest);
+
+    List<Option> options = getOption(orderRequest);
+
+    BigDecimal price =
+        jeep.getBasePrice().add(color.getPrice()).add(engine.getPrice()).add(tire.getPrice());
+
+    for (Option option : options) {
+      price.add(option.getPrice());
     }
 
-    /**
-     *
-     * @param orderRequest
-     * @return
-     */
-    protected Tire getTire(OrderRequest orderRequest) {
-        return jeepOrderDao.fetchTire(orderRequest.getTire())
-                .orElseThrow(() -> new NoSuchElementException(
-                        "Tire with ID=" + orderRequest.getTire() + " was not found"));
-    }
+    return jeepOrderDao.saveOrder(customer, jeep, color, engine, tire, price, options);
+  }
 
-    /**
-     *
-     * @param orderRequest
-     * @return
-     */
-    protected Engine getEngine(OrderRequest orderRequest) {
-        return jeepOrderDao.fetchEngine(orderRequest.getEngine())
-                .orElseThrow(() -> new NoSuchElementException(
-                        "Engine with ID=" + orderRequest.getEngine() + " was not found"));
-    }
-
-    /**
-     *
-     * @param orderRequest
-     * @return
-     */
-    protected Color getColor(OrderRequest orderRequest) {
-        return jeepOrderDao.fetchColor(orderRequest.getColor())
-                .orElseThrow(() -> new NoSuchElementException(
-                        "Color with ID=" + orderRequest.getColor() + " was not found"));
-    }
-
-    /**
-     *
-     * @param orderRequest
-     * @return
-     */
-    protected Jeep getModel(OrderRequest orderRequest) {
-        return jeepOrderDao
-                .fetchModel(orderRequest.getModel(), orderRequest.getTrim(),
-                        orderRequest.getDoors())
-                .orElseThrow(() -> new NoSuchElementException("Model with ID="
-                        + orderRequest.getModel() + ", trim=" + orderRequest.getTrim()
-                        + orderRequest.getDoors() + " was not found"));
-    }
-
-    /**
-     *
-     * @param orderRequest
-     * @return
-     */
-    protected Customer getCustomer(OrderRequest orderRequest) {
-        return jeepOrderDao.fetchCustomer(orderRequest.getCustomer())
-                .orElseThrow(() -> new NoSuchElementException("Customer with ID="
-                        + orderRequest.getCustomer() + " was not found"));
-    }
-
-    @Transactional
-    @Override
-    public Order createOrder(OrderRequest orderRequest) {
-          Optional<Customer> customer = jeepOrderDao.fetchCustomer(orderRequest.getCustomer());
-//
-     Optional<Jeep> jeep = jeepOrderDao.fetchModel(orderRequest.getModel(), orderRequest.getTrim(), orderRequest.getDoors());
-
-     Optional<Color> color = jeepOrderDao.fetchColor(orderRequest.getColor());
-
-     Optional<Engine> engine = jeepOrderDao.fetchEngine(orderRequest.getEngine());
-
-     Optional<Tire> tire = jeepOrderDao.fetchTire(orderRequest.getTire());
-
-        return null;
-    }
-
-
-//    @Override
-//    public Order createOrder(OrderRequest orderRequest) {
-//        Customer customer = jeepOrderDao.fetchCustomer(orderRequest.getCustomer());
-//
-////        Jeep jeep = jeepOrderDao.fetchModel
-//
-//        return null;
-//    }
+  //    @Override
+  //    public Order createOrder(OrderRequest orderRequest) {
+  //        Customer customer = jeepOrderDao.fetchCustomer(orderRequest.getCustomer());
+  //
+  ////        Jeep jeep = jeepOrderDao.fetchModel
+  //
+  //        return null;
+  //    }
 }
